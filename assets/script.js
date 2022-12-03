7bc52dfeb11c7eef899dcca68fc2c4f5e93f62db
Shaka Suzy <shaheedchambers@gmail.com>
2022-12-02 20:18:01 -0500

    starter JS

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var time = 120;
+var startSection = document.getElementById("start-section");
+var startButton = document.getElementById("start-button");
+var questionSection = document.getElementById("question-section");
+var question
+
+
+//Question array
+var questionArray = [
+    {
+        question: "this is question 1", 
+        choices: ["a","b","c","d"],
+        answer: "b"
+    },
+    //fill out more questions beneath 
+]
